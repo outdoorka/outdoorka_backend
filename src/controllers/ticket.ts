a386ea889d6e364142c48bc8cb88c1ef4707debf
Wendy <wen1224@gmail.com>
2024-06-14 16:35:12 +0800

    chore: ticketController move back to ticket file

--- src/controllers/ticket.ts
+++ src/controllers/ticket.ts
@@ -1,6 +1,6 @@
 import { handleResponse, handleAppError } from '../services/handleResponse';
 import type { NextFunction, Request, Response } from 'express';
-import { TicketModel } from '../models';
+import { TicketModel, PaymentModel } from '../models';
 import { type JwtPayloadRequest } from '../types/dto/user';
 import { status400Codes, status404Codes, status500Codes } from '../types/enum/appStatusCode';
 import { Types } from 'mongoose';
@@ -114,5 +114,90 @@ export const ticketController = {
       return;
     }
     handleResponse(res, { ticketStatus: 1 }, '驗票成功');
+  },
+
+  async getOwnerTicketData(req: Request, res: Response, next: NextFunction): Promise<void> {
+    const userId = (req as JwtPayloadRequest).user._id;
+
+    // 查詢使用者購買的付款記錄
+    const payments = await PaymentModel.find({ buyer: userId, paymentStatus: 'paid' }).select(
+      '_id'
+    );
+
+    // 提取付款payment ID
+    const paymentIds = payments.map((payment: any) => payment._id);
+
+    const ticketData = await TicketModel.aggregate([
+      {
+        $match: {
+          $or: [{ owner: userId }, { payment: { $in: paymentIds } }]
+        }
+      },
+      {
+        $lookup: {
+          from: 'payments',
+          localField: 'payment',
+          foreignField: '_id',
+          as: 'paymentInfo'
+        }
+      },
+      {
+        $unwind: '$paymentInfo'
+      },
+      {
+        $lookup: {
+          from: 'activities',
+          localField: 'activity',
+          foreignField: '_id',
+          as: 'activityInfo'
+        }
+      },
+      {
+        $unwind: '$activityInfo'
+      },
+      {
+        $group: {
+          _id: '$activity',
+          activityInfo: { $first: '$activityInfo' },
+          paymentInfo: { $first: '$paymentInfo' },
+          tickets: {
+            $push: {
+              ticketId: '$_id',
+              ticketStatus: '$ticketStatus',
+              ticketOwnerId: '$owner'
+            }
+          }
+        }
+      },
+      {
+        $project: {
+          _id: 1,
+          title: '$activityInfo.title',
+          bookedCapacity: '$activityInfo.bookedCapacity',
+          region: '$activityInfo.region',
+          city: '$activityInfo.city',
+          activityImageUrl: { $first: '$activityInfo.activityImageUrls' },
+          activityStartTime: '$activityInfo.activityStartTime',
+          activityEndTime: '$activityInfo.activityEndTime',
+          // likers: { $size: '$activityInfo.likers' },
+          paymentId: '$paymentInfo._id',
+          paymentBuyer: '$paymentInfo.buyer',
+          ticketCount: { $size: '$tickets' },
+          tickets: 1
+        }
+      }
+    ]).exec();
+
+    if (!ticketData) {
+      handleAppError(
+        404,
+        status404Codes[status404Codes.NOT_FOUND_TICKET],
+        status404Codes.NOT_FOUND_TICKET,
+        next
+      );
+      return;
+    }
+
+    handleResponse(res, ticketData, '取得成功');
   }
 };
